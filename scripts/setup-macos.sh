1d649ae932879d63c47da1583693597d961fa29c
Monte Hellawell <monte@montudor.com>
2024-09-15 22:14:15 +0100

    feat(setup): add dock items support

--- scripts/setup-macos.sh
+++ scripts/setup-macos.sh
@@ -32,6 +32,83 @@ defaults write com.apple.dock show-recents -bool false;
 defaults write com.apple.dock mru-spaces -bool true
 defaults write -g AppleSpacesSwitchOnActivate -bool false
 defaults write com.apple.dock tilesize -int 28
+
+create_dock_item() {
+    local app_path="$1"
+    echo "<dict>
+        <key>tile-data</key>
+        <dict>
+            <key>file-data</key>
+            <dict>
+                <key>_CFURLString</key>
+                <string>$app_path</string>
+                <key>_CFURLStringType</key>
+                <integer>0</integer>
+            </dict>
+        </dict>
+    </dict>"
+}
+
+create_folder_item() {
+    local folder_path="$1"
+    local arrangement="$2"
+    local displayas="$3"
+    local showas="$4"
+    echo "<dict>
+        <key>tile-data</key>
+        <dict>
+            <key>arrangement</key>
+            <integer>$arrangement</integer>
+            <key>displayas</key>
+            <integer>$displayas</integer>
+            <key>file-data</key>
+            <dict>
+                <key>_CFURLString</key>
+                <string>file://$folder_path</string>
+                <key>_CFURLStringType</key>
+                <integer>15</integer>
+            </dict>
+            <key>showas</key>
+            <integer>$showas</integer>
+        </dict>
+        <key>tile-type</key>
+        <string>directory-tile</string>
+    </dict>"
+}
+
+apps=(
+    "/System/Applications/Launchpad.app"
+    "/System/Applications/Mission Control.app"
+    "/Applications/Safari.app"
+    "/Applications/Arc.app"
+    "/Applications/kitty.app"
+    "/Applications/Visual Studio Code - Insiders.app"
+    "/Applications/Docker.app"
+    "/Applications/UTM.app"
+    "/Applications/ChatGPT.app"
+    "/Applications/Discord.app"
+    "/System/Applications/Messages.app"
+    "/System/Applications/FaceTime.app"
+    "/System/Applications/Mail.app"
+    "/System/Applications/Photos.app"
+    "/System/Applications/Calendar.app"
+    "/System/Applications/Reminders.app"
+    "/System/Applications/Notes.app"
+    "/System/Applications/Music.app"
+)
+
+defaults write com.apple.dock persistent-apps -array
+for app in "${apps[@]}"; do
+    defaults write com.apple.dock persistent-apps -array-add "$(create_dock_item "$app")"
+done
+
+
+
+defaults write com.apple.dock persistent-others -array
+defaults write com.apple.dock persistent-others -array-add "$(create_folder_item "/Users/$USER/Documents/" 1 1 2)"
+defaults write com.apple.dock persistent-others -array-add "$(create_folder_item "/Users/$USER/Downloads/" 2 2 1)"
+
+
 killall Dock
 
 
@@ -70,6 +147,7 @@ fi
 # Install Homebrew packages from Brewfile
 echo "Installing homebrew packages..."
 brew bundle --file="$(dirname "$0")/../brew/Brewfile-core" || true
+killall Dock
 
 
 # Tmux TPM Install
