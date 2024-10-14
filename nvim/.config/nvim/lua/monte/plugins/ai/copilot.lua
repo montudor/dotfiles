2e44975d74fff96040c31ae0657608774ad3ca8e
Monte Hellawell <monte@montudor.com>
2024-10-14 23:04:12 +0100

    feat(nvim): try out avante and copilot

---
+++ nvim/.config/nvim/lua/monte/plugins/ai/copilot.lua
@@ -0,0 +1,53 @@
+return {
+	"zbirenbaum/copilot.lua",
+	cmd = "Copilot",
+	event = { "BufReadPost", "InsertEnter" },
+	config = function()
+		require("copilot").setup({
+			panel = {
+				enabled = true,
+				auto_refresh = false,
+				keymap = {
+					jump_prev = "[[",
+					jump_next = "]]",
+					accept = "<CR>",
+					refresh = "gr",
+					open = false,
+				},
+				layout = {
+					position = "bottom", -- | top | left | right
+					ratio = 0.4,
+				},
+			},
+			suggestion = {
+				enabled = true,
+				auto_trigger = true,
+				hide_during_completion = true,
+				debounce = 75,
+				keymap = {
+					accept = "<C-u>",
+					accept_word = false,
+					accept_line = false,
+					next = "<M-]>",
+					prev = "<M-[>",
+					dismiss = "<C-]>",
+				},
+			},
+			filetypes = {
+				yaml = false,
+				markdown = false,
+				help = false,
+				gitcommit = false,
+				gitrebase = false,
+				hgcommit = false,
+				svn = false,
+				cvs = false,
+				["."] = false,
+			},
+			copilot_node_command = "node", -- Node.js version must be > 18.x
+			server_opts_overrides = {},
+		})
+
+		vim.api.nvim_set_keymap("n", "<Leader>acp", ":Copilot panel<CR>", { noremap = true, silent = true })
+	end,
+}
