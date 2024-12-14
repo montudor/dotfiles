446da02ea47c6c3e0b17aa40fc37f64d8204a491
Monte Hellawell <monte@montudor.com>
2024-12-14 14:28:54 +0000

    feat(nvim): telescope previous search utils

--- nvim/.config/nvim/lua/monte/plugins/telescope.lua
+++ nvim/.config/nvim/lua/monte/plugins/telescope.lua
@@ -19,6 +19,8 @@ return {
 						["<C-k>"] = actions.move_selection_previous, -- move to prev result
 						["<C-j>"] = actions.move_selection_next, -- move to next result
 						["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+						["<C-p>"] = actions.cycle_history_prev,
+						["<C-n>"] = actions.cycle_history_next,
 					},
 				},
 			},
@@ -27,7 +29,7 @@ return {
 		telescope.load_extension("fzf")
 
 		vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
-		vim.keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
+		vim.keymap.set("n", "<leader>fo", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
 		vim.keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
 		vim.keymap.set(
 			"n",
@@ -40,5 +42,8 @@ return {
 		vim.keymap.set("n", "<leader>fh", "<cmd>Telescope git_status<cr>", { desc = "Find git status" })
 		vim.keymap.set("n", "<leader>fd", "<cmd>Telescope diagnostics<cr>", { desc = "Find diagnostic issues" })
 		vim.keymap.set("n", "<leader>fm", "<cmd>Telescope keymaps<cr>", { desc = "Find keymaps" })
+
+		--resume last search
+		vim.keymap.set("n", "<leader>fr", "<cmd>Telescope resume<cr>", { desc = "Resume last search" })
 	end,
 }
