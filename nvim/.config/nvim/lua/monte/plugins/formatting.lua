835b15231743545c7e3248aa905ecbd886d1d020
Monte Hellawell <monte@montudor.com>
2025-09-19 19:39:16 +0100

    feat(nvim): htmlangular using prettier

--- nvim/.config/nvim/lua/monte/plugins/formatting.lua
+++ nvim/.config/nvim/lua/monte/plugins/formatting.lua
@@ -14,6 +14,7 @@ return {
 				vue = { "prettier" },
 				css = { "prettier" },
 				html = { "prettier" },
+				htmlangular = { "prettier" },
 				json = { "prettier" },
 				yaml = { "prettier" },
 				markdown = { "prettier" },
